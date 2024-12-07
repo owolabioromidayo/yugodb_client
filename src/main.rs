e18a936ef39d07c9acb845ff4b72637a2a940070
Oromidayo Owolabi <owolabioromidayo16@gmail.com>
2024-12-07 18:47:42 +0100

    db queries from client side

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::{TcpStream};
+use std::net::{Shutdown, TcpStream};
 use std::io::{Read, Write};
 use std::str::from_utf8;
 
@@ -7,23 +7,101 @@ fn main() {
         Ok(mut stream) => {
             println!("Successfully connected to server in port 3333");
 
-            let msg = b"Hello!";
+            let msg = b"
+                    dbs.create_db('test_db');
+        dbs.create_table('test_db' ,'test_table', 'DOCUMENT', 'ROW');
 
-            stream.write(msg).unwrap();
-            println!("Sent Hello, awaiting reply...");
+        dbs.create_table('test_db' ,'test_rtable', 'RELATIONAL', 'ROW', '{
+            'name': 'string(50)',
+            'balance': ['numeric', true],
+            'pob': 'string',
+            'active': 'boolean'
+        }');
 
-            let mut data = [0 as u8; 1024]; // using 6 byte buffer
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                 
-                    let text = from_utf8(&data).unwrap();
-                    println!("Reply: {}", text);
-                    
+        dbs.insert('test_db', 'test_table', '{ 
+                'name': 'John Doe',
+                'age': 30.0,
+                'city': 'New York',
+                'address': {
+                    'street': '123 Main St',
+                    'zip': '10001'
                 },
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
+                'phone_numbers': [
+                    '123-456-7890',
+                    '987-654-3210'
+                ]
+        }');
+
+        dbs.insert('test_db', 'test_table', '{
+            'name': 'Jane Smith',
+            'age': 25.0,
+            'city': 'London',
+            'address': {
+                'street': '456 High St',
+                'zip': 'SW1A 1AA'
+            },
+            'phone_numbers': [
+                '020-1234-5678'
+            ],
+            'employment': {
+                'company': 'Acme Inc.',
+                'position': 'Software Engineer',
+                'start_date': {
+                'year': 2022.0,
+                'month': 1.0
                 }
             }
+            }');
+
+            dbs.insert('test_db', 'test_rtable', '{
+                'name': 'Jane Smith',
+                'balance': '2502034304.2332',
+                'pob': 'London',
+                'active': true
+            }');
+
+            dbs.insert('test_db', 'test_rtable', '{
+                'name': 'John Doe',
+                'balance': '450.2332',
+                'pob': 'New York',
+                'active': false
+            }');
+       
+
+
+        let x = dbs.test_db.test_table.offset(0);  
+        let y = dbs.test_db.test_rtable.offset(0);  
+        // x.limit(10);
+        // y.limit(10);
+        let z  = x LJOIN y ON name=name;
+        z.limit(10);
+            
+            ";
+
+        let msg_len = (msg.len() as u32).to_be_bytes();
+        stream.write_all(&msg_len).unwrap();
+        stream.write_all(msg).unwrap();
+            println!("Sent Hello, awaiting reply...");
+
+            let mut buffer = Vec::new();
+
+            if let Err(e) = stream.read_to_end(&mut buffer) {
+                eprintln!("Failed to receive data from server: {}", e);
+                return;
+            }
+        
+            match String::from_utf8(buffer){
+                Ok(s) => {
+                    println!("We got a response: {:?}", s); 
+                }
+                Err(e) => {
+                    // eprintln!("Invalid UTF-8 data from client: {}", e);
+                    // let _ = stream.shutdown(Shutdown::Both);
+                    stream.write(format!("Could not decode response :( {:?}", e).as_bytes()).unwrap();
+                    stream.shutdown(Shutdown::Both).unwrap();
+        
+                }
+            };
         },
         Err(e) => {
             println!("Failed to connect: {}", e);
